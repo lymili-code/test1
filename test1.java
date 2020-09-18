402a1b20476145453605db9d49e7f892f7edfb75
lymili-code <71482630+lymili-code@users.noreply.github.com>
2020-09-18 22:07:58 +0800

    创建test1.java文件
    第二次使用GitHub创建项目

---
+++ test1.java
@@ -0,0 +1,17 @@
+/**
+   *文档注释
+   *这是一个打印hello world的类
+   *@author 左右
+   *@version 1.0.0
+   */
+public class Test{
+      public static void  main(String[] args){
+                //单行注释：打印hello world
+                System.out.print("hello world");
+                /*
+                多行注释
+                下面写一个打印abc的程序
+               */
+               System.out.print("abc");
+        } 
+}
